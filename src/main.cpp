81e55e687387f23c3ce06604ee7e5046e26aac86
unknown <jakubzajac0222@gmail.com>
2025-12-16 04:46:57 +0100

    added shader class, basic shaders, added glad to cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,58 @@
+
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include "Shader.h"
+#include <iostream>
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
 
 int main()
 {
     glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    
+
+    std::cout << "okno";
     GLFWwindow* window = glfwCreateWindow(800, 600, "Test", nullptr, nullptr);
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    std::cout << "shadery";
+    const char *vertexPath = "../../shaders/vertex.vs";
+    const char *fragmentPath = "../../shaders/fragment.fs";
+    Shader shader(vertexPath, fragmentPath);
+
+    std::cout << "glm";
     glm::vec3 v(1.0f, 2.0f, 3.0f);
+
+    std::cout << "okno2";
 
     while (!glfwWindowShouldClose(window))
     {
+
+        shader.use();
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    std::cout << "koniec";
     glfwTerminate();
     return 0;
 }
